1b5ed1ebab9ca5804b416a1a99ff5a860110c3f3
imaginaryfeud <dagne.work@gmail.com>
2026-04-14 17:57:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+import discord
 from discord.ext import commands
-import ping3
+from mcstatus import JavaServer
 import os
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -10,12 +14,12 @@ async def on_ready():
 
 @bot.command()
 async def status(ctx):
-    ip = "5.20.187.185:25565"  # change to your server IP
-    response = ping3.ping(ip, timeout=2)
+    try:
+        server = JavaServer.lookup("5.20.187.185:25565")
+        status = server.status()
 
-    if response:
-        await ctx.send("🟢 Online")
-    else:
+        await ctx.send(f"🟢 Online - {status.players.online}/{status.players.max} players")
+    except:
         await ctx.send("🔴 Offline")
 
 bot.run(os.getenv("TOKEN"))
